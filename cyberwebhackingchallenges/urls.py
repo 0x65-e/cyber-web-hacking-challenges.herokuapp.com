0b26c05bc4f535c403f1f8e311edc7f6a59fe07b
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-10-10 21:52:22 -0700

    Added sample robots.txt

--- cyberwebhackingchallenges/urls.py
+++ cyberwebhackingchallenges/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
